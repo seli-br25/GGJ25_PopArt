7c936b562dd0485ad46b0883d0f90b4ab263a280
Selina Breuer <selina.breuer@tuwien.ac.at>
2025-01-24 22:30:16 +0100

    finished gameplay prototype with json

--- PopArt/Assets/Resources/Scripts/BubbleManager.cs
+++ PopArt/Assets/Resources/Scripts/BubbleManager.cs
@@ -1,25 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BubbleManager : MonoBehaviour
 {
     public Color bubbleColor;
+    public string targetComponent;
     private Vector2 movementDirection;
     public float speed = 5f;
 
-    // Start is called before the first frame update
     void Start()
     {
         movementDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
         GetComponent<SpriteRenderer>().color = bubbleColor;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(movementDirection * speed * Time.deltaTime);
-
         Vector3 position = Camera.main.WorldToViewportPoint(transform.position);
         if (position.x < 0 || position.x > 1) movementDirection.x = -movementDirection.x;
         if (position.y < 0 || position.y > 1) movementDirection.y = -movementDirection.y;
@@ -27,7 +23,19 @@ public class BubbleManager : MonoBehaviour
 
     private void OnMouseDown()
     {
-        GameManager.Instance.FillShape(bubbleColor, transform.position);
-        Destroy(gameObject);
+        int layerMask = LayerMask.GetMask("Shapes");
+        Collider2D hitCollider = Physics2D.OverlapPoint(transform.position, layerMask);
+
+        if (hitCollider != null)
+        {
+            Debug.Log($"Kollision erkannt mit: {hitCollider.transform.name}");
+            GameManager.Instance.FillShape(hitCollider.transform.name, bubbleColor);
+            Destroy(gameObject); 
+        }
+        else
+        {
+            Debug.Log("Keine Kollision erkannt.");
+        }
     }
+
 }
--- PopArt/Assets/Resources/Scripts/GameManager.cs
+++ PopArt/Assets/Resources/Scripts/GameManager.cs
@@ -1,11 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class Artwork
+{
+    public int id;
+    public string title;
+    public List<ComponentData> components;
+}
+
+[System.Serializable]
+public class ComponentData
+{
+    public string name;
+    public string correctColor;
+    public bool isRightColor;
+}
+
+[System.Serializable]
+public class ArtworksData
+{
+    public List<Artwork> artworks;
+}
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance; 
-    public GameObject bubblePrefab; 
-    public Transform[] shapes;
+    public static GameManager Instance;
+    public GameObject bubblePrefab;
+    public Transform artworkContainer;
     public int bubbleCount = 20;
+
+    private Artwork currentArtwork;
+    private List<Color> bubbleColors = new List<Color>();
 
     private void Awake()
     {
@@ -14,43 +40,84 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        for (int i = 0; i < bubbleCount; i++)
+        LoadArtwork(1);
+        SpawnBubbles();
+    }
+
+    void LoadArtwork(int id)
+    {
+        // Load JSON
+        TextAsset jsonFile = Resources.Load<TextAsset>("artworks");
+        ArtworksData data = JsonUtility.FromJson<ArtworksData>(jsonFile.text);
+
+        currentArtwork = data.artworks.Find(a => a.id == id);
+
+        foreach(var component in currentArtwork.components)
         {
-            SpawnBubble();
-        }
-    }
+            bubbleColors.Add(HexToColor(component.correctColor));
 
-    void SpawnBubble()
-    {
-        Vector2 randomPosition = new Vector2(Random.Range(-8f, 8f), Random.Range(-4f, 4f));
-        GameObject bubble = Instantiate(bubblePrefab, randomPosition, Quaternion.identity);
-        bubble.GetComponent <BubbleManager>().bubbleColor = GetRandomColor();
-    }
-
-    Color GetRandomColor()
-    {
-        Color[] colors = { Color.red, Color.green, Color.blue };
-        return colors[Random.Range(0, colors.Length)];
-    }
-
-    public void FillShape(Color color, Vector2 position)
-    {
-        Transform nearestShape = null;
-        float minDistance = float.MaxValue;
-
-        foreach (Transform shape in shapes)
-        {
-            float distance = Vector2.Distance(position, shape.position);
-            if (distance < minDistance)
+            Transform child = artworkContainer.Find(component.name);
+            if (child != null)
             {
-                minDistance = distance;
-                nearestShape = shape;
+                child.GetComponent<SpriteRenderer>().color = Color.white;
             }
         }
+    }
 
-        if (nearestShape != null)
+    void SpawnBubbles()
+    {
+        for (int i = 0; i < bubbleCount; i++)
         {
-            nearestShape.GetComponent<SpriteRenderer>().color = color;
+            Vector2 randomPosition = new Vector2(Random.Range(-8f, 8f), Random.Range(-4f, 4f));
+            GameObject bubble = Instantiate(bubblePrefab, randomPosition, Quaternion.identity);
+            int randomIndex = Random.Range(0, currentArtwork.components.Count);
+            var component = currentArtwork.components[randomIndex];
+            bubble.GetComponent<BubbleManager>().bubbleColor = HexToColor(component.correctColor);
+            bubble.GetComponent<BubbleManager>().targetComponent = component.name;
+        }
+    }
+
+    Color HexToColor(string hex)
+    {
+        if (ColorUtility.TryParseHtmlString(hex, out Color color))
+        {
+            return color;
+        }
+        return Color.white;
+    }
+
+    public void FillShape(string shapeName, Color color)
+    {
+        Transform shape = artworkContainer.Find(shapeName);
+        if (shape != null)
+        {
+            SpriteRenderer shapeRenderer = shape.GetComponent<SpriteRenderer>();
+            shapeRenderer.color = color;
+
+            var component = currentArtwork.components.Find(c => c.name == shapeName);
+            if (component != null)
+            {
+                if (shapeRenderer.color == HexToColor(component.correctColor))
+                {
+                    component.isRightColor = true;
+                    Debug.Log($"{shapeName} hat die richtige Farbe!");
+                }
+                else
+                {
+                    Debug.Log($"{shapeName} hat NICHT die richtige Farbe!");
+                }
+            }
+
+            CheckWinCondition();
+        }
+    }
+
+    void CheckWinCondition()
+    {
+        // check if all components are colored correctly
+        if (currentArtwork.components.TrueForAll(c => c.isRightColor))
+        {
+            Debug.Log("Du hast gewonnen!");
         }
     }
 }
